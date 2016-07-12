8979f0e334c4c32bec037ae74c35c2349c0a9dbc
Amy <amyyuexiao@gmail.com>
2016-07-11 21:43:03 -0400

    index.html added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-require('.app/routes')(app);
+require('./app/routes')(app);
 
 app.listen(port);
 console.log('Listening on port ' + port);
